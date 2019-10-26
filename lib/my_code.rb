f093a84bf4554ae91c61ff7d8c1373dd23aaf468
José Romero <jdromerov88@gmail.com>
2019-10-26 04:36:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,11 @@
 # My Code here....
 def map_to_negativize(source_array)
-  pp source_array
+  #pp source_array
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
 end
